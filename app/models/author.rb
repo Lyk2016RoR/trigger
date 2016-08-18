d76e42edc90cffe76fb5be48e726bd274e931af5
OnurIzmitlioglu <izmitliogluonur@gmail.com>
2016-08-18 20:52:24 +0300

    Model relations are added.

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,4 +2,6 @@ class Author < ApplicationRecord
   validates :name, presence: true
   validates :surname, presence: true
   validates :profile, presence: true
+
+  has_many :books
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -18,6 +18,7 @@ class Book < ApplicationRecord
 	belongs_to :category
 	has_many :comments
 	belongs_to :user, optional: true
+	belongs_to :author
 	
 
 end
