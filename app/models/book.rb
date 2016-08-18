5f5e477815f7858838137b2ee62c2b2811ddb26d
OnurIzmitlioglu <izmitliogluonur@gmail.com>
2016-08-18 14:13:20 +0300

    Book Controller is generated.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id           :integer          not null, primary key
+#  name         :string
+#  description  :text
+#  published_at :integer
+#  publisher    :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Book < ApplicationRecord
+	validates :name, presence: true
+	validates :description, presence: true
+	
+
 end
