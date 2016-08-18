b45d202b73f98816f073bfe34da75d2c7e5b7d13
Simge Şen <simge.sen@bil.omu.edu.tr>
2016-08-18 16:27:47 +0300

    add coment şeysileri

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,37 @@
 class CommentsController < ApplicationController
+
+ 
+  before_action :set_book
+  before_action :set_comment, only: [:destroy]
+ 
+  def create
+    @comment = @book.comments.new(comment_params)
+    
+
+  def destroy
+    @comment.destroy
+    redirect_to @book, notice: "Comment was deleted"
+  end
+
+  private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
+  def set_book
+    @book = Book.find(params[:book_id])
+  end
+
+end
+
+
+
+
+
 end
