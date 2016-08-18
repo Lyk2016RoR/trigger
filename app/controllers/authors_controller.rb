4a34a212df76008652b20f101de9f63cd898c9c0
DilaraHayta <1306130091@ogr.iu.edu.tr>
2016-08-18 14:23:04 +0300

    Author model and controller created.

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,24 @@
+class AuthorsController < ApplicationController
+  def index
+
+  end
+  def new
+
+  end
+  def show
+
+  end
+  def edit
+
+  end
+  def update
+
+  end
+  def create
+
+  end
+  def delete
+
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160818104429) do
+ActiveRecord::Schema.define(version: 20160818110947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.string   "surname"
+    t.string   "profile"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string   "name"
