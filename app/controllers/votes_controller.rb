5e741119b36b9db28b332439fd42fbf528ca4011
DilaraHayta <1306130091@ogr.iu.edu.tr>
2016-08-18 21:37:00 +0300

    Vote controller and model changed.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,6 +2,16 @@ class VotesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_book
   def create
+
+    @vote = @book.votes.new
+        @vote.rating = params[:vote][:rating]
+        @vote.user = current_user
+
+        if @vote.save
+          redirect_to @book, notice: "Vote was saved."
+        else
+          redirect_to @book, notice: "Vote is not valid."
+        end
   end
 
   def destroy
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,6 @@
 class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :book
+  validates :rating, inclusion: (1..5)
+  validates :user_id, uniqueness: {scope: :book_id}
 end
