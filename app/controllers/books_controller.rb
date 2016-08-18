4ac9bb00a4bc9a061931c4cf2b849d8b7fea50c7
OnurIzmitlioglu <izmitliogluonur@gmail.com>
2016-08-18 19:43:15 +0300

    Edit,update,... methods for book controller.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,15 +12,38 @@ class BooksController < ApplicationController
 		@book = Book.find(params[:id])
 	end
 	def edit
-
+		@book = Book.find(params[:id])
 	end
 	def update
+		@book = Book.find(params[:id])
+
+		if @book.update(book_params)
+			redirect_to book_path(@book)
+		else
+			render :edit
+		end
 
 	end
+	
 	def create
+		@book = Book.find(params[:id])
 
+		if @book.save
+			redirect_to book_path(@book)
+		end
 	end
+
+
 	def delete
 
 	end
+
+
+	private
+
+
+	def book_params
+		params.permit(:name, :description, :published_at, :publisher)
+	end 
+
 end
